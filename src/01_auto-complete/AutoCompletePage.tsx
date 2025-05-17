8345a2243e87cf79066217635d9b61b4827c4f5d
eo9on9 <eo9on9@gmail.com>
2025-05-17 16:27:28 +0900

    Add popover

--- src/01_auto-complete/AutoCompletePage.tsx
+++ src/01_auto-complete/AutoCompletePage.tsx
@@ -1,9 +1,14 @@
 'use client'
 
-import { useState } from 'react'
+import * as Popover from '@radix-ui/react-popover'
+import { useRef, useState } from 'react'
 import { getSuggestions } from './getSuggestions'
 
 export const AutoCompletePage = () => {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const [isOpen, setIsOpen] = useState(false)
+
   const [keyword, setKeyword] = useState('')
 
   const [options, setOptions] = useState<string[]>([])
@@ -11,20 +16,48 @@ export const AutoCompletePage = () => {
   const handleClick = async () => {
     const [_, suggestions] = await getSuggestions(keyword)
 
+    setIsOpen(true)
+
     setOptions(suggestions)
   }
 
   return (
     <div>
-      <input value={keyword} onChange={e => setKeyword(e.target.value)} />
       <button onClick={handleClick}>suggestion</button>
-      <ul>
-        {options.map(o => (
-          <li key={o} onClick={() => setKeyword(o)}>
-            {o}
-          </li>
-        ))}
-      </ul>
+      <div>
+        <input
+          ref={inputRef}
+          style={{
+            width: '100%',
+          }}
+          value={keyword}
+          onChange={e => setKeyword(e.target.value)}
+        />
+        <Popover.Root open={isOpen} onOpenChange={setIsOpen}>
+          <Popover.Trigger asChild>
+            <div />
+          </Popover.Trigger>
+          <Popover.Portal>
+            <Popover.Content onOpenAutoFocus={e => e.preventDefault()}>
+              <ul>
+                {options.map(o => (
+                  <li
+                    key={o}
+                    onClick={() => {
+                      setKeyword(o)
+                      setIsOpen(false)
+
+                      inputRef.current?.focus()
+                    }}
+                  >
+                    {o}
+                  </li>
+                ))}
+              </ul>
+            </Popover.Content>
+          </Popover.Portal>
+        </Popover.Root>
+      </div>
     </div>
   )
 }
