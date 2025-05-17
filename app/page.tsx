c7aa80835495cf07dcc40aa7a44b9fa4469c88a1
eo9on9 <eo9on9@gmail.com>
2025-05-17 10:41:45 +0900

    Create

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return 'Home'
+  return (
+    <div>
+      <Link href="/01_auto-complete">01 Auto Complete</Link>
+    </div>
+  )
 }
---
+++ src/01_auto-complete/pages/AutoCompletePage.tsx
@@ -0,0 +1,1 @@
+export const AutoCompletePage = () => 'Auto Complete Page'
