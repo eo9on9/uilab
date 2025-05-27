3dcc19551492e6ea4d75e09d7cabfae42233ea5f
eo9on9 <eo9on9@gmail.com>
2025-05-27 10:13:02 +0900

    Rename

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <Link href="/01_auto-complete">01 Auto Complete</Link>
+      <Link href="/01-auto_complete">01 Auto Complete</Link>
     </div>
   )
 }
